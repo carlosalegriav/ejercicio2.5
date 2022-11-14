0295ebad8fd1f84db1f0dd318cfcfb248fb4d5c0
Lenovo <Lenovo@DESKTOP-620RG2R>
2022-11-13 21:22:30 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio2.5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ejerciciodoscinco
+{
+
+    public class lote
+    {
+        public static void Main()
+        {
+            decimal a, b, c, at, ar, ato;
+            Console.WriteLine("Dijite la longitud de A");
+            a=decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Dijite la longitud de B");
+            b=decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Dijite la longitud de C");
+            c=decimal.Parse(Console.ReadLine());
+            ar = b * c;
+            at = ((a - c) * b) / 2;
+            ato = ar + at;
+            Console.WriteLine($"El area calculada es : {ato}");
+         }
+    }
+}
